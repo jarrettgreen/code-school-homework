01defd00f30c1770867342e447e0cf3d96917226
Jarrett Green <jarrett@balancedcomp.com>
2014-07-29 21:52:56 -0500

    added first md

---
+++ js-branches/something.md
@@ -0,0 +1,3 @@
+# This is a markdown file
+
+Isn't it FUCKING AWESOME?! HOLY SHIT!
--- templates/jquery-basic/assets/javascripts/code-school.js
+++ templates/jquery-basic/assets/javascripts/code-school.js
@@ -1,13 +1,24 @@
 var insertInstructions = function(){
   $('body').prepend('<div id="instructions"></div> <span id="instructions-toggle"> + Instructions </span>');
+    var xhr = new XMLHttpRequest();
+    var converter = new Showdown.converter();
+
+    xhr.open('get', 'http://192.241.221.238/something.md', true);
+
+    xhr.onload = function(e) {
+      var insructionsMarkedDown = converter.makeHtml(xhr.responseText);
+      $('#content').html(insructionsMarkedDown);
+    };
+
+    xhr.send();
 }
+
 $(function(){
   insertInstructions();
+
   $('#instructions').slideToggle();
   $('#instructions-toggle').click(function(){
     $('#instructions').slideToggle();
   })
-
-
 
 })
