20655693740426e50a51f5c00ccbb03b1bae7b92
Jarrett Green <jarrett@balancedcomp.com>
2014-07-29 22:57:19 -0500

    updated instructions

--- js-branches/instructions.js
+++ js-branches/instructions.js
@@ -3,8 +3,11 @@
     "<hr/>" +
     "<h3> &rarr; Objective: Create a page that asks the user which type of animal they would like read about -  based on their answer, we will show them information about that animal</h3>" +
     "<p> In the html below, you'll find 3 section tags. Each section has information regarding 3 different animals - Lions, Tigers, and Bears </p>" +
+    "<p> Don't worry about loading jQuery. I've done it for you. Look in the head - you'll see how. </p>" +
+
     "<p> <ol> " +
     "<li> Hide the sections when the page loads </li>" +
     "<li> Create a prompt that opens when the page loads, asking the user what kind of animal they would like to read about. </li>" +
     "<li> Write a conditional branch that will evaluate the text they provided, and if it mathces 'Lions', 'Tigers', or 'Bears', show the appropriate section </li> " +
-    "<li> If you don't have a match (they type 'fox') alert the user saying so, and start the process over."
+    "<li> If you don't have a match (they type 'fox') alert the user saying so, and start the process over." +
+    "</ol></p>"
